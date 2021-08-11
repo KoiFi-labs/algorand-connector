7e25e3fbc3de16a95989c2774c50db2831edfafa
Peladito <estebanlucianorey@gmail.com>
2021-08-11 12:11:04 -0300

    fix if no global state is set

--- application/entities/getApp/getApp.entity.js
+++ application/entities/getApp/getApp.entity.js
@@ -4,11 +4,11 @@ exports.getApp = ({getApp}) => async (params) => {
     try{
         let app = await getApp(params)
         if(!app || !app.application) throw "App not found"
-        app.globalState = app.application.params['global-state'].map(({key,value})=>{
+        app.globalState = app.application.params['global-state']?app.application.params['global-state'].map(({key,value})=>{
             let o = {}
             o[Buffer.from(key, 'base64')]=value.type===2?value.uint:value.bytes
             return o
-        })
+        }):{}
         
         
         return app
