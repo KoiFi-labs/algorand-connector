f5ddf93952fda8bd205fc1e748a11d45ce62da21
Peladito <estebanlucianorey@gmail.com>
2021-08-11 11:58:37 -0300

    adding getAppState uc

--- application/entities/getApp/getApp.entity.js
+++ application/entities/getApp/getApp.entity.js
@@ -2,7 +2,16 @@ const { GetAppError } = require("./errors");
 
 exports.getApp = ({getApp}) => async (params) => {
     try{
-        return await getApp(params)
+        let app = await getApp(params)
+        if(!app || !app.application) throw "App not found"
+        app.globalState = app.application.params['global-state'].map(({key,value})=>{
+            let o = {}
+            o[Buffer.from(key, 'base64')]=value.type===2?value.uint:value.bytes
+            return o
+        })
+        
+        
+        return app
     }catch(error){
         throw new GetAppError(error)
     }
